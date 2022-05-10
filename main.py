30c3dda87b777c01bfcf9d19422949e4857bd2fd
aldairbalanzar <46764787+aldairbalanzar@users.noreply.github.com>
2022-05-09 20:03:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from tempfile import NamedTemporaryFile
 from typing import List
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from utils.s3 import s3
-from utils.security import validate_file_size
+from utils.security import validate_file_size, validate_file_type
+from PIL import Image
+from io import BytesIO
 
 app = FastAPI()
 print('\t>>> API is runnning...')
@@ -12,10 +15,26 @@ def read_root():
     return {"greeting": "waddup"}
 
 @app.post('/')
-async def add_file(files: List[UploadFile] = File(...)):
-    if validate_file_size(files):
-        print('\t>>>adding file(s)...')
-        for file in files:
-            s3.resource.Bucket(s3.aws_bucket).put_object(Key=f'images/{file.filename}')
+async def add_files(files: List[UploadFile] = File(...)):
+    for file in files:
+        data = await file.read()
+        file_copy = NamedTemporaryFile(delete=False)
 
-    return {'message': 'file(s) have been uploaded.'}
+        file_copy.write(data)
+        file_copy.seek(0)
+        validate_file_type(file)
+        validate_file_size(file_copy)
+
+    print('\t>>> adding file(s)...')
+    for file in files:
+        data = await file.read()
+        file_copy = NamedTemporaryFile(delete=False)
+
+        file_copy.write(data)
+        file_copy.seek(0)
+        
+        s3.resource.Bucket(s3.aws_bucket).put_object(Body=data, Key=f'images/{file.filename}',  ContentType=file.content_type)
+        # s3.resource.Bucket(s3.aws_bucket).upload_file(file.filename, f'images/{file.filename}', ExtraArgs={'ContentType': file.content_type})
+
+
+    return {'message': 'successfuly added files!'}
