ff1a6a5e5fa48ba738798a7db42f7673576b67ce
aldairbalanzar <46764787+aldairbalanzar@users.noreply.github.com>
2022-05-09 20:04:11 -0400

    Update security.py

--- utils/security.py
+++ utils/security.py
@@ -1,14 +1,20 @@
 from fastapi import File, UploadFile, HTTPException
-from typing import List
 
-def validate_file_size(files: List[UploadFile] = File(...)):
+def validate_file_size(file: UploadFile = File(...)):
     limit=2_000_000
     file_size = 0
-    for file in files:
-        for chunk in file.file:
-            file_size += len(chunk)
-            if file_size > limit:
-                print(f'\t>>> file {file.filename} is too big.')
-                raise HTTPException(status_code=404, detail='file size too big.')
-    
+    for chunk in file:
+        file_size += len(chunk)
+        if file_size > limit:
+            print(f'\t>>> file {file.filename} is too big.')
+            raise HTTPException(status_code=404, detail='file size too big.')
+
     return True
+
+def validate_file_type(file: UploadFile = File(...)):
+    print(f'type: {file.content_type}')
+    if file.content_type == 'image/png' or file.content_type == 'image/jpeg':
+        return True
+
+    print(f'\t>>> file {file.filename} is of wrong type.')
+    raise HTTPException(status_code=404, detail='file is of wrong type.')
